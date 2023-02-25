e329ff2c7b88d384bad93e8f3795234e27a19f10
Léo Colombaro <git@colombaro.fr>
2023-02-25 00:55:00 +0000

    Use terraform

---
+++ tf/main.tf
@@ -0,0 +1,45 @@
+variable "cloudflare_account_id" {
+  type = string
+  sensitive = true
+}
+
+variable "cloudflare_api_token" {
+  type = string
+  sensitive = true
+}
+
+variable "sentry_dsn" {
+  type = string
+  sensitive = true
+}
+
+terraform {
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "3.31.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
+resource "cloudflare_pages_project" "dispoflare_pages_project" {
+  account_id = var.cloudflare_account_id
+  name              = "dispoflare"
+  production_branch = "main"
+
+  deployment_configs {
+    production {
+      environment_variables = {
+        CLOUDFLARE_ACCOUNT_ID = sensitive(var.cloudflare_account_id)
+        CLOUDFLARE_API_TOKEN = sensitive(var.cloudflare_api_token)
+        SENTRY_DSN = sensitive(var.sentry_dsn)
+      }
+
+      compatibility_date = "2023-02-25"
+    }
+  }
+}
