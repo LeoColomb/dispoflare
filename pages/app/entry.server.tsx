d628fc8c6f68440910eb0a1b0b68db730273bc49
Léo Colombaro <git@colombaro.fr>
2023-02-25 14:47:20 +0000

    Try

--- pages/app/entry.server.tsx
+++ pages/app/entry.server.tsx
@@ -1,33 +1,27 @@
 import type { EntryContext } from '@remix-run/cloudflare'
 import { PassThrough } from 'stream'
-import { renderToPipeableStream } from 'react-dom/server'
+import { renderToReadableStream } from 'react-dom/server'
 import { RemixServer } from '@remix-run/react'
 
-export default function handleRequest(
+export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
-  return new Promise((resolve, reject) => {
-    const { pipe } = renderToPipeableStream(
-      <RemixServer context={remixContext} url={request.url} />,
-      {
-        onShellReady() {
-          responseHeaders.set('Content-Type', 'text/html')
-          const body = new PassThrough()
-          resolve(
-            new Response(body, {
-              status: responseStatusCode,
-              headers: responseHeaders,
-            }),
-          )
-          pipe(body)
-        },
-        onShellError(err: unknown) {
-          reject(err)
-        },
+  const body = await renderToReadableStream(
+    <RemixServer context={remixContext} url={request.url} />,
+    {
+      onError() {
+        responseStatusCode = 500
       },
-    )
+    },
+  )
+
+  responseHeaders.set('Content-Type', 'text/html')
+
+  return new Response(body, {
+    status: responseStatusCode,
+    headers: responseHeaders,
   })
 }
