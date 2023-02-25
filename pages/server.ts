603eda5256d12d42d3b6057af194764cc6cd1afe
Léo Colombaro <git@colombaro.fr>
2023-02-25 14:10:25 +0000

    Full typescript

--- pages/server.ts
+++ pages/server.ts
@@ -1,7 +1,7 @@
 import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages'
 import * as build from '@remix-run/dev/server-build'
 
-const handleRequest = createPagesFunctionHandler({
+export const onRequest: PagesFunction<Env> = createPagesFunctionHandler({
   build,
   mode: process.env.NODE_ENV,
   getLoadContext: (context) => ({
@@ -9,22 +9,3 @@ const handleRequest = createPagesFunctionHandler({
     CF: (context.request as any).cf as IncomingRequestCfProperties | undefined,
   }),
 })
-
-export default {
-  fetch: async (
-    request: Request,
-    env: Env,
-    context: ExecutionContext,
-  ): Promise<Response> =>
-    handleRequest({
-      env,
-      params: {},
-      request: new Request(request),
-      waitUntil: context.waitUntil,
-      next: () => {
-        throw new Error('next() called in Worker')
-      },
-      functionPath: '',
-      data: undefined,
-    }),
-}
