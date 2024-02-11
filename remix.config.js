40165ed1bb8759b4efe91fad4330bc287c872d5e
Léo Colombaro <git@colombaro.fr>
2024-02-11 21:13:24 +0000

    Use Pico v2

--- remix.config.js
+++ remix.config.js
@@ -15,12 +15,4 @@ export default {
   serverNodeBuiltinsPolyfill: {
     modules: {},
   },
-  future: {
-    v2_dev: true,
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
 }
